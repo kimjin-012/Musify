2d46714c614f3a35fda808fc9cbb7e57fa76156e
kimjin-012 <77659317+kimjin-012@users.noreply.github.com>
2021-04-08 20:24:23 -0700

    Musify Setting up
    Musify Setting up Django environment(djangoEnv) and setting up all Django project and app files.

---
+++ djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\na020\onedrive\desktop\mygit\musify\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
